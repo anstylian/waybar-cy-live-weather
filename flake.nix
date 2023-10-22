56ea9459c3d9f12d62f8f010d0bfc3cedcddbcc4
Agathangelos Stylianidis <agathangelos.stylianidis@gmail.com>
2023-10-22 14:14:14 +0300

    update crates. package the crate as nix package

--- flake.nix
+++ flake.nix
@@ -1,52 +1,36 @@
 {
-  description = "Example Rust development environment for Zero to Nix";
-
-  # Flake inputs
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixpkgs-unstable"; # also valid: "nixpkgs"
-    rust-overlay.url = "github:oxalica/rust-overlay"; # A helper for Rust + Nix
+    cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
+    flake-utils.follows = "cargo2nix/flake-utils";
+    nixpkgs.follows = "cargo2nix/nixpkgs";
   };
 
-  # Flake outputs
-  outputs = { self, nixpkgs, rust-overlay }:
-    let
-      # Overlays enable you to customize the Nixpkgs attribute set
-      overlays = [
-        # Makes a `rust-bin` attribute available in Nixpkgs
-        (import rust-overlay)
-        # Provides a `rustToolchain` attribute for Nixpkgs that we can use to
-        # create a Rust environment
-        (self: super: {
-          rustToolchain = super.rust-bin.stable.latest.default;
-        })
-      ];
-
-      # Systems supported
-      allSystems = [
-        "x86_64-linux" # 64-bit Intel/AMD Linux
-        "aarch64-linux" # 64-bit ARM Linux
-        "x86_64-darwin" # 64-bit Intel macOS
-        "aarch64-darwin" # 64-bit ARM macOS
-      ];
-
-      # Helper to provide system-specific attributes
-      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
-        pkgs = import nixpkgs { inherit overlays system; };
-      });
-    in
-    {
-      # Development environment output
-      devShells = forAllSystems ({ pkgs }: {
-        default = pkgs.mkShell {
-          # The Nix packages provided in the environment
-          packages = (with pkgs; [
-            # The package provided by our custom overlay. Includes cargo, Clippy, cargo-fmt,
-            # rustdoc, rustfmt, and other tools.
-            rustToolchain
-            openssl
-            pkg-config
-          ]) ++ pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs; [ libiconv ]);
+  outputs = inputs: with inputs;
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ cargo2nix.overlays.default ];
         };
-      });
-    };
+
+        rustPkgs = pkgs.rustBuilder.makePackageSet {
+          rustVersion = "1.73.0";
+          packageFun = import ./Cargo.nix;
+        };
+
+        workspaceShell = rustPkgs.workspaceShell {
+          packages = with pkgs; [ pkg-config openssl ];
+        };
+      in
+      rec {
+        devShells = {
+          default = workspaceShell;
+        };
+
+        packages = {
+          waybar-cy-live-weather = (rustPkgs.workspace.waybar-cy-live-weather { });
+          default = packages.waybar-cy-live-weather;
+        };
+      }
+    );
 }
